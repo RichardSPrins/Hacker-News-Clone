71e25549bed5e871b4cb759244c09876ef7f1571
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-07-14 12:39:38 -0700

    folder structure set up, starting code

--- hacker-news-clone/src/index.js
+++ hacker-news-clone/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import GlobalStyles from './styles/globals'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderApp = () => {
+  ReactDOM.render(
+    <div>
+      <GlobalStyles />
+      <App />
+    </div>
+    ,document.getElementById('root')
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+renderApp();
